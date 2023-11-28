e0f2b3c07d53da157d30177dc801131ebfbcb968
zahra sarabi <zahrasarabi1379@gmail.com>
2023-11-28 12:05:58 +0330

    add posts Reducer to the store

---
+++ src/app/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from "../features/posts/postsSlice";
+
+export const store = configureStore({
+  reducer: { posts: postsReducer },
+});
