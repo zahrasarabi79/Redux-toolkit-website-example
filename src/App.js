8cc9171ad0c9bc0e0803b41e038b0d829c013b95
zahra sarabi <zahrasarabi1379@gmail.com>
2023-12-02 15:54:58 +0330

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import PostsLists from "./features/posts/PostsLists";
 import AddPostForm from "./features/posts/AddPostForm";
-
+import SinglePostPage from "./features/posts/SinglePostPage";
+import Layout from "./Components/Layout";
+import { Route, Routes } from "react-router-dom";
+import EditPostForm from "./features/posts/EditPostForm";
 function App() {
   return (
-    <main className="App">
-      <AddPostForm />
-      <PostsLists />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsLists />} />
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
